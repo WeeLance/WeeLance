579c0d1ff0bdeaf226c9bcdb5cfa712ae9c65840
WiemMimouni <wiemmimouni74@gmail.com>
2023-01-03 10:34:58 +0100

    hello

--- WeeLance/component/home.js
+++ WeeLance/component/home.js
@@ -2,16 +2,17 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, Avatar } from 'native-base';
  
 function HomeScreen({ navigation }) {
     return (
       
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          onPress={() => navigation.navigate('Notifications')}
-          title="Go to notifications"
-        />
+        <Avatar bg="green.500" source={{
+      uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        AJ
+      </Avatar>
 
       </View>
        
