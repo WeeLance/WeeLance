b29d1c0a9e5b51facc0343e12c58718d63e241cd
unknown <zayatsunami@gmail.com>
2023-01-09 17:30:07 +0000

    done

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express"); 
 const router = express.Router() ;
-const {addClient,getAll} = require("../controllers/clientController" ) ;
+const {addClient,getAll,updateOne} = require("../controllers/clientController" ) ;
 
 router.post("/addClient", addClient) ; 
 router.get("/getAll",getAll)  ; 
+router.put("/updateOne/:id",updateOne);
 
 module.exports = router ;
